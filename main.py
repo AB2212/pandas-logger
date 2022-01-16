4674aaf890a689d1dcf05ecb8b468ff58fb09c62
abhibiswas <abhijeetbiswas@gmail.com>
2022-01-16 22:29:33 +0530

    add disabling

--- main.py
+++ main.py
@@ -4,9 +4,8 @@ from pandas_logger import enable_pandas_logging, disable_pandas_logging
 def random_operations(df):
     df = df.drop_duplicates()
     df = df.drop_duplicates(subset="b")
-    df1 = df.merge(df, how="outer", on="a")
+    df = df.merge(df, how="outer", on="a")
     df.sort_values("a", inplace=True)
-    df = pd.merge(df, df, on="a")
     df.dropna(inplace=True)
     df = pd.Series([1, 3, 4])
     df = df.isnull()
@@ -14,12 +13,10 @@ def random_operations(df):
 
 
 if __name__ == "__main__":
-    # Add these lines
     print("Enabling Logging")
     enable_pandas_logging()
     df = pd.DataFrame([[1, -2], [3, 4], [3, pd.NA]], columns=["a", "b"])
     print(random_operations(df.copy()))
-    print(pd.test('fast'))
-    # print("Disabling Logging")
-    # disable_pandas_logging()
-    # print(random_operations(df))
+    print("Disabling Logging")
+    disable_pandas_logging()
+    print(random_operations(df))
